d8bed049d8c6ff62297936c0c7f56c6b1e5c9e48
gabriela rodriquez <garc2004@gmail.com>
2023-06-18 14:44:05 -0400

    all

---
+++ client/src/redux/reducer/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    // Define tu estado inicial aquí
+  };
+  
+  const reducer = (state = initialState, action) => {
+    // Maneja las acciones y actualiza el estado en consecuencia
+    return state;
+  };
+  
+  export default reducer;
+  
