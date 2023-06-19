0a5fb8845bcb58c59946705e8ae0a07be2da9a64
gabriela rodriquez <garc2004@gmail.com>
2023-06-18 20:29:41 -0400

    all

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,1 +1,11 @@
-const GET_COUNTRIES = 'GET_COUNTRIES'
+import axios from 'axios'
+
+export const GET_COUNTRIES = 'GET_COUNTRIES'
+
+export function getCountries() {
+    return async (dispatch) => {
+        const res = await axios.get ("http://localhost:3001/countries");
+        dispatch({ type:GET_COUNTRIES, payload: res.data })
+        console.log('aqui esta la info:')
+    }
+}
--- client/src/redux/reducer/reducer.js
+++ client/src/redux/reducer/reducer.js
@@ -1,14 +1,22 @@
+import { GET_COUNTRIES } from '../actions/actions'
+
+
 const initialState = {
     countries: []
   };
   
   const reducer = (state = initialState, action) => {
     switch (action.type) {
-    
-      default:return state
-        break;
-    }
+      case GET_COUNTRIES:{
+        return{
+          ...state,
+          countries: action.payload
+        }
+      }
+      default:
+        return state;
   };
+}
   
   export default reducer;
   
