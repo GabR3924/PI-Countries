7ce7eb373350ebd5ac990bf8508d3af87b165400
gabriela rodriquez <garc2004@gmail.com>
2023-06-20 00:13:01 -0400

    all

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,10 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getCountries,setPage } from "./redux/actions/actions";
+import { useEffect } from "react";
 import Countries from "./components/Countries/Countries";
 import "./App.css";
+import usePagination from "./usePagination/usePagination";
 
 function App() {
-  const dispatch = useDispatch();
-  const countries = useSelector((state) => state.countries);
-  const page = useSelector((state) => state.page)
-  const countriesPerPage = 12;
-  const startIndex = page * countriesPerPage;
-  const endIndex = startIndex + countriesPerPage;
-  const visibleCountries = countries.slice(startIndex, endIndex);
-  const totalPages = Math.ceil(countries.length / countriesPerPage);
-
-  useEffect(() => {
-    dispatch(getCountries());
-  }, [dispatch]);
-
-  const handleNextPage = () => {
-    dispatch(setPage(page + 1)); 
-  };
-  
-  const handlePrevPage = () => {
-    dispatch(setPage(Math.max(page - 1, 0)));
-  };
-  
-
+  const { visibleCountries,page, totalPages, handleNextPage, handlePrevPage } = usePagination();
 
   return (
     <>
