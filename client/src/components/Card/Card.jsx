afdaf9dd47768822b8b9a106af4751e3da7b569b
gabriela rodriquez <garc2004@gmail.com>
2023-06-29 02:50:12 -0400

    all

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -4,9 +4,14 @@ import style from "./Card.module.css";
 const Card = ({ name, flag, continent, onClick }) => {
   return (
     <div className={style.card} onClick={onClick}>
-      <h2>{name}</h2>
-      <h4>continente: {continent}</h4>
+      <dic className={style.img}>
       <img src={flag} alt={`Bandera de ${name}`} />
+      </dic>
+      <div className={style.txt}>
+
+      <h2>{name}</h2>
+      <h4>{continent}</h4>
+      </div>
     </div>
   );
 };
